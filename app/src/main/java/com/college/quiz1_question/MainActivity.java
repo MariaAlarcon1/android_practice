efc5f3de486a1fe6187a15274f510c9a4a0b7395
Maria Alarcon <maria.alarcong1@gmail.com>
2022-06-22 01:36:08 -0400

    Almost completed

--- app/src/main/java/com/college/quiz1_question/MainActivity.java
+++ app/src/main/java/com/college/quiz1_question/MainActivity.java
@@ -1,12 +1,24 @@
 package com.college.quiz1_question;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ShareCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mShareTextEditText;
     //EditText for Dial action
     private EditText mDialEditText;
+    private String contact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +43,35 @@ public class MainActivity extends AppCompatActivity {
         mShareTextEditText = findViewById(R.id.share_edittext);
         mDialEditText = findViewById(R.id.phone_edittext);
 
+        Button openWebsiteBtn = findViewById(R.id.open_website_button);
+        Button openLocationBtn = findViewById(R.id.open_location_button);
+        Button shareTextBtn = findViewById(R.id.share_text_button);
+        Button buttonCall = findViewById(R.id.dial_button);
+
+
+        //Open a URL in a browser.
+        openWebsiteBtn.setOnClickListener((v)->{openWebsite(openWebsiteBtn);});
+
+        openLocationBtn.setOnClickListener((v)->{openLocation(openLocationBtn);});
+        shareTextBtn.setOnClickListener((v)->{ shareText(mShareTextEditText);});
+
+        buttonCall.setOnClickListener((v)->{dial(mDialEditText);});
+
+        /*
+    It contains buttons for sending implicit intents:
+     - Open a URL in a browser.
+     - Find a location on a map.
+     - Share a text string.
+     - View the Contacts
+     - Dial a number
+     */
+
+        //Find a location on a map.
     }
 
     // TODO
+
+
     /**
      * Handles the onClick for the "Open Website" button.  Gets the URI
      * from the edit text and sends an implicit intent for that URL.
@@ -40,6 +79,9 @@ public class MainActivity extends AppCompatActivity {
      * @param view The view (Button) that was clicked.
      */
     public void openWebsite(View view) {
+        String url = mWebsiteEditText.getText().toString();
+        Intent openWebsiteIntent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(url));
+        startActivity(openWebsiteIntent);
     }
 
     // TODO
@@ -52,7 +94,12 @@ public class MainActivity extends AppCompatActivity {
      * @param view The view (Button) that was clicked.
      */
     public void openLocation(View view) {
+        String location = mLocationEditText.getText().toString();
+        //https://developers.google.com/maps/documentation/urls/android-intents
+        String locationToSearch = "geo:0,0?q=" + location;
 
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(locationToSearch));
+        this.startActivity(intent);
     }
 
     // TODO
@@ -66,7 +113,12 @@ public class MainActivity extends AppCompatActivity {
      * @param view The view (Button) that was clicked.
      */
     public void shareText(View view) {
-
+        //https://guides.codepath.com/android/Sharing-Content-with-Intents
+        String message = mShareTextEditText.getText().toString();
+            Intent shareTextIntent = new Intent(Intent.ACTION_SEND);
+            shareTextIntent.setType("text/plain");
+            shareTextIntent.putExtra(Intent.EXTRA_TEXT, message);
+            startActivity(Intent.createChooser(shareTextIntent, "Share text using: "));
     }
 
     // TODO
@@ -76,6 +128,9 @@ public class MainActivity extends AppCompatActivity {
      * @param view The view (Button) that was clicked.
      */
     public void dial(View view) {
+        String phoneNumber = findViewById(R.id.phone_edittext).toString();
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        dialIntent.setData(Uri.parse(phoneNumber));
     }
 
     // TODO
@@ -85,7 +140,26 @@ public class MainActivity extends AppCompatActivity {
      * @param view The view (Button) that was clicked.
      */
     public void viewContact(View view) {
+        Intent viewContactsIntent = new Intent(Intent.ACTION_PICK);
+        viewContactsIntent.setType("");
+        requestContactsPermission();
+    }
 
+    private boolean hasContactsPermission()
+    {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) ==
+                PackageManager.PERMISSION_GRANTED;
+    }
+
+    // Request contact permission if it
+    // has not been granted already
+    private void requestContactsPermission()
+    {
+        if (!hasContactsPermission())
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CONTACTS}, 0);
+        }
     }
 
 }
